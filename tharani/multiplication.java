f0b89a90994c2e59452e2dbeb7ee14ff6f06901f
tharanimurugesan <m.tharani.193@gmail.com>
2020-02-25 18:02:29 +0530

    #multiplication table program

---
+++ tharani/multiplication.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main
+{
+
+	public static void main(String[] args) {
+		
+		int n,i,j;
+ 
+   		
+		Scanner sc=new Scanner(System.in);
+   		
+		System.out.println("Enter the first input:");
+  
+   		
+		n=sc.nextInt();
+   		
+		System.out.println("Enter the second input:");
+  
+  
+ 		i=sc.nextInt();
+   
+		
+for(j=1;j<=i;j++)
+
+   
+		{
+    
+   	
+			System.out.println(n+"*"+j+"="+n*j);
+  
+	                }
+  
+ 
+
+
+	  }
+
+}
