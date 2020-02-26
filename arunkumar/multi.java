16ab5f0e7a8cf673b38f7fb6400dedbf810219c3
ArunkumarAk6 <arunmithran2020@gmail.com>
2020-02-26 10:10:43 +0530

    #multiplication program

---
+++ arunkumar/multi.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+public class multi
+{
+	     
+	    static void show(int m,int l)
+        {
+          int p;
+          if(l>0 && m>0)
+          {	
+	        for(int i=1;i<=l;i++)
+	        {
+             p=m*i;
+	         System.out.println(m+"*"+i+"="+p);
+	        }
+	      }
+	      
+	     } 
+        public static void main(String[] args)
+         {
+          while(true)
+           {	
+             try
+           {
+           
+          
+            int m,l,p;
+	        System.out.println("The number");
+	        Scanner in=new Scanner(System.in);
+            m=in.nextInt(); 
+            System.out.println("The limitation number");
+            l=in.nextInt();
+            show(m,l);
+            break;
+	      
+	       }
+
+	     catch(InputMismatchException e)
+	     {
+	     	System.out.println("\n enter valid inputs");
+	     }
+	   }
+	   }  
+
+}	    
+	  
+
+	 
+	
+	
