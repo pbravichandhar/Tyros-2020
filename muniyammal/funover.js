096332927cd1e99a4e5067583bba25701ad00152
Muniyammal <vinayagajo25@gmail.com>
2020-03-03 15:06:10 +0530

    functionarg.js

---
+++ muniyammal/funover.js
@@ -0,0 +1,87 @@
+/*This program implements the function overloading
+*prints the standard rresult as output
+*/
+//overload the function 
+function bakya(a,b,c)
+{
+    if(c['test'])
+    {
+        console.log(a+b);
+    }
+    else{
+        console.log(a*b);
+    }
+}
+//function call with different arguments
+bakya(1,2,{"method":"adds"});
+bakya(3,4,{"test":"Equals","bar":"tree"});
+
+function sayHi() {   // (1) create
+    return "Hello";
+  }
+  //function expression...
+  
+  let func = sayHi();    // (2) copy
+  
+  console.log(func); // Hello  
+  //assign the function to the variable   
+  let a=sayHi();
+  //prints the variable.....
+  console.log(a);
+  //callback functions....
+  function ask(question,yes,no){
+      //question variable equal to the string it returns true
+      if(question == "do you agree")
+      {
+          yes();
+        }
+      else{
+          no();
+      }
+  }
+  //if the question true then showOk executed.....
+  function showOk()
+  {
+      console.log("you agreed...");
+  }
+  //if it is false showCancel executed.......
+  function showCancel()
+  {
+      console.log("Your order cancelled....");
+  }
+  ask("hello",showOk,showCancel);
+  //function declaration...
+  function sumCheck(a,b){
+    return a+b;
+}
+let res=sumCheck(10,20);
+console.log(res);
+//function expression
+sayHi("John");
+function sayHi(name){
+    console.log(`hello ,${name}`);
+}
+//console.log(sayHello);
+var standard_input=process.stdin;
+standard_input.setEncoding('utf-8');
+console.log("Enter the age");
+standard_input.on('data',function(data){
+    let welcome=(data>18)?()=>{ console.log("Hello");}:()=>{console.log("Greetings....");}
+    welcome();
+});
+//arrow functions
+//let func={fun1,fun2,fun3,.........}=>expressions
+let sum=(a,b) => a+b;
+//prints the arrow function....
+console.log(sum(10,20));
+let double=(n)=>n*2;
+console.log(double(5));
+let sayBye=()=>console.log("Hello");
+sayBye();
+//multiline arrow function....
+let sub=(a,b)=>{
+    let res=a*b;
+    return res;
+}
+console.log(sub(30,5));
+
