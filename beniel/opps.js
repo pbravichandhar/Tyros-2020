97741a2c61a47b019fb58addc1afa630e77c1ab0
ben004 <benbeniel007@gmail.com>
2020-03-05 12:58:07 +0530

    oops concept in js

--- beniel/opps.js
+++ beniel/opps.js
@@ -1,34 +1,28 @@
 // adding properties to constructor
-function Constr(name,initial)
-{
-    this.name=name;
-    this.initial=initial;
+function Constr(name, initial) {
+    this.name = name;
+    this.initial = initial;
 }
-Constr.prototype.age=21;
+Constr.prototype.age = 21;
 // parameter constructor in js
-var obj=new Constr("beniel","D");
+var obj = new Constr("beniel", "D");
 console.log(obj);
 console.log(obj.age);
 // class creation in js
-class parent
-{
-    constructor(name )
-    {
-        this.name=name;
+class parent {
+    constructor(name) {
+        this.name = name;
     }
-    display()
-    {
-    console.log(this.name);
+    display() {
+        console.log(this.name);
     }
-    static static_function(){
+    static static_function() {
         console.log("static function inside the class");
     }
 }
 // inheritance from parent to child
-class child extends parent
-{
-    constructor(name)
-    {
+class child extends parent {
+    constructor(name) {
         super(name);
     }
 }
@@ -38,30 +32,32 @@ obj3.display();
 // static function inside the class called by classname.function name
 parent.static_function();
 // constructor stealing
-function constructor1(){
-    this.carbrands = ['BMW','FORD','AUDI'];
+function constructor1() {
+    this.carbrands = ['BMW', 'FORD', 'AUDI'];
 }
+
 function constructor1_sub() {
     constructor1.call(this);
 }
-let inst=new constructor1_sub();
-console.log("car brands "+inst.carbrands);
+let inst = new constructor1_sub();
+console.log("car brands " + inst.carbrands);
 
 // inheritance in constructor
-function main(name , location){
-    this.name =name;
+function main(name, location) {
+    this.name = name;
     this.location = location;
 }
-main.prototype.getvalues=function(){
-    console.log(this.name+"   "+this.location);
+main.prototype.getvalues = function() {
+    console.log(this.name + "   " + this.location);
 }
-const maii_obj=new main("calibraint","tharamani");
+const maii_obj = new main("calibraint", "tharamani");
 
-sub.prototype=Object.create(main.prototype);
-function sub(name , location,area )
-{
-    main.call(this,name,location);
-    this.area=area;
+sub.prototype = Object.create(main.prototype);
+
+function sub(name, location, area) {
+    main.call(this, name, location);
+    this.area = area;
 }
-let sub_obj=new sub("calibraint","tharamani");
+let sub_obj = new sub("calibraint", "tharamani");
+// call the main constructor function by sub class object
 sub_obj.getvalues();
