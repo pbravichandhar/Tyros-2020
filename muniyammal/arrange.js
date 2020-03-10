476a343e52350d0d4ee09d32055888a42ff17ae8
Muniyammal <vinayagajo25@gmail.com>
2020-03-10 11:58:44 +0530

    combination of numbers

---
+++ muniyammal/arrange.js
@@ -0,0 +1,37 @@
+const arrange = require("readline") 
+const a =arrange. createInterface({
+    input : process . stdin,
+    output : process . stdout
+})
+a.question("enter the no of elements",(n)=>{
+    let count = 0;
+      n = ['h','b','c'];
+      const arrange = (n,c)=> {
+        if (c == n.length) 
+        {
+            for (let i = 0; i < n.length; i++) 
+            {
+                  console.log(" [" + n[i] + "] ");
+            }
+            count++;
+        } 
+        else 
+        {
+            for (let i = c; i < n.length; i++) 
+            {
+                let temp = n[c];
+                n[c] = n[i];
+                n[i] = temp;
+                
+                arrange(n, c + 1);
+    
+                temp = n[c];
+                n[c] = n[i];
+                n[i] = temp;
+            }
+        }
+    }
+    arrange(n,0);
+        console.log("combination of the elements" + n + "are" + count);
+        a.close();
+})
